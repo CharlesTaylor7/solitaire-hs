761de8bef01e380f22082562b820d995359bb682
Charles Taylor <charlestaylor95@gmail.com>
2019-10-06 23:21:19 -0400

    Test loopM

--- test/Solitaire/UtilsSpec.hs
+++ test/Solitaire/UtilsSpec.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE TypeApplications #-}
 module Solitaire.UtilsSpec where
 
-import Solitaire
+import Data.Monoid
+
+import Control.Monad.Trans.State.Strict
 import Test.QuickCheck
 import Test.Hspec
+
+import Solitaire
 
 data DummyEnum = A | B | C
   deriving (Enum, Bounded)
@@ -23,3 +27,27 @@ spec = do
         enumSize @DummyEnum A `shouldBe` 3
       it "does not evaluate its argument" $ do
         enumSize @DummyEnum undefined `shouldBe` 3
+    describe "loopM" $ do
+      it "enables branching in the list monad" $ do
+        let act x =
+              if x <= 0
+              then [Right (x + 1)]
+              else
+                [ Left (x - 1)
+                , Left (x - 2)
+                , Right (x + 1)
+                ]
+
+        loopM act 3 `shouldBe` [1, 0, 2, 1, 3, 1, 0, 2, 4]
+      it "enables imperative style while loops in the writer monad" $ do
+        let factorial x =
+              if x <= 0
+              then pure $ Right ()
+              else (Product x, Left (x - 1))
+        loopM factorial 4 `shouldBe` (Product 24, ())
+      it "what does it do in the reader monad?" $ do
+        let try x = \n ->
+              if x > 10
+              then Right x
+              else Left (n+x)
+        (loopM try 2) 4 `shouldBe` 14
