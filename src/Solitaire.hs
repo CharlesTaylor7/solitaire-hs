5a3e923f2565235c277c4b9e8429834e599331a1
Charles Taylor <charlestaylor95@gmail.com>
2019-10-12 18:48:37 -0400

    Move actions to separate file

--- src/Solitaire.hs
+++ src/Solitaire.hs
@@ -8,6 +8,7 @@ module Solitaire
   ( module Solitaire
   , module Solitaire.Utils
   , module Solitaire.Types
+  , module Solitaire.Actions
   , module M
   , module V
   , module MV
@@ -38,6 +39,7 @@ import System.Random
 import Solitaire.PrettyPrinter
 import Solitaire.Types
 import Solitaire.Utils
+import Solitaire.Actions
 
 -- Pure declarations
 
@@ -72,71 +74,6 @@ numCards = enumSize @Card
 
 initialPileSize :: Int
 initialPileSize = length deck `div` numPiles
-
-isSuccessorOf :: Card -> Card -> Bool
-a `isSuccessorOf` b =
-  fromEnum a - fromEnum b == 1
-
-countWhile :: Foldable f => (a -> Bool) -> f a -> Int
-countWhile p =
-  let
-    reducer x acc
-      | p x = 1 + acc
-      | otherwise = acc
-  in foldr reducer 0
-
-splitAtStack :: Vector Card -> (Vector Card, Vector Card)
-splitAtStack cards =
-  maybe
-    (V.empty, V.empty)
-    (\(head, rest) ->
-      let
-        n =
-          mzip rest cards
-            & countWhile (uncurry isSuccessorOf)
-      in V.splitAt (n+1) cards
-    )
-    (uncons cards)
-
-moveReducer :: Move -> Game -> Game
-moveReducer move =
-  case normalize 3 move of
-    MoveStack (MS i j) ->
-      let
-        move layout =
-          let
-            (stack, rest) =
-              layout ^?! ix i . faceUp . to splitAtStack
-            source' = ix i . faceUp .~ rest
-            target' = ix j . faceUp <>~ stack
-          in
-            layout & source' . target'
-
-      in layout . _Layout %~ move
-
-    FlipCard (FC i) ->
-      let
-        flipCard pile =
-          let
-            (head, rest) = pile ^?! faceDown . _Cons
-            faceUp' = faceUp .~ [head]
-            faceDown' = faceDown .~ rest
-          in
-            pile & faceUp' . faceDown'
-      in
-        layout . _Layout . ix i %~ flipCard
-
-    MoveToFoundation (MTF i) ->
-      let
-        updateLayout layout =
-          let
-            pile = layout ^?! ix i
-            pile' = pile & faceUp %~ (view (to splitAtStack . _2))
-          in
-            layout
-      in
-        (layout . _Layout %~ updateLayout)
-          . (foundation . numSets +~ 1)
 
 -- effectful
 shuffleIO :: Foldable f => f a -> IO [a]
