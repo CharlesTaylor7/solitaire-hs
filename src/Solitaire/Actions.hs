bcd0d26cf1994c08744122b6e83f31395da35949
Charles Taylor <charlestaylor95@gmail.com>
2019-10-12 18:52:15 -0400

    Add actions

---
+++ src/Solitaire/Actions.hs
@@ -0,0 +1,76 @@
+module Solitaire.Actions where
+
+import Control.Monad.Zip
+
+import Control.Lens
+
+import Data.IntMap (IntMap)
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+
+import Solitaire.Types
+
+isSuccessorOf :: Card -> Card -> Bool
+a `isSuccessorOf` b =
+  fromEnum a - fromEnum b == 1
+
+countWhile :: Foldable f => (a -> Bool) -> f a -> Int
+countWhile p =
+  let
+    reducer x acc
+      | p x = 1 + acc
+      | otherwise = acc
+  in foldr reducer 0
+
+splitAtStack :: Vector Card -> (Vector Card, Vector Card)
+splitAtStack cards =
+  maybe
+    (mempty, mempty)
+    (\(head, rest) ->
+      let
+        n =
+          mzip rest cards
+            & countWhile (uncurry isSuccessorOf)
+      in V.splitAt (n+1) cards
+    )
+    (uncons cards)
+
+moveReducer :: Move -> Game -> Game
+moveReducer move =
+  case normalize 3 move of
+    MoveStack (MS i j) ->
+      let
+        move layout =
+          let
+            (stack, rest) =
+              layout ^?! ix i . faceUp . to splitAtStack
+            source' = ix i . faceUp .~ rest
+            target' = ix j . faceUp <>~ stack
+          in
+            layout & source' . target'
+
+      in layout . _Layout %~ move
+
+    FlipCard (FC i) ->
+      let
+        flipCard pile =
+          let
+            (head, rest) = pile ^?! faceDown . _Cons
+            faceUp' = faceUp .~ [head]
+            faceDown' = faceDown .~ rest
+          in
+            pile & faceUp' . faceDown'
+      in
+        layout . _Layout . ix i %~ flipCard
+
+    MoveToFoundation (MTF i) ->
+      let
+        updateLayout layout =
+          let
+            pile = layout ^?! ix i
+            pile' = pile & faceUp %~ (view (to splitAtStack . _2))
+          in
+            layout
+      in
+        (layout . _Layout %~ updateLayout)
+          . (foundation . numSets +~ 1)
