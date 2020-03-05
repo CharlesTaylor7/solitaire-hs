509b834b9134782058df7b0dc99479dbc8d7bfb5
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-03-05 16:49:52 -0500

    Use ReaderT to thread env variable

--- src/Solitaire/Actions.hs
+++ src/Solitaire/Actions.hs
@@ -9,16 +9,15 @@ import Solitaire.Types
 import Solitaire.Utils
 import qualified Data.Vector as V
 
-numPiles :: Int
-numPiles = 3
-
-moves :: [Move]
-moves = moves ++ flips ++ sets
-  where
+moves :: (MonadReader Env m) => m [Move]
+moves = do
+  numPiles <- view env_numPiles <$> ask
+  let
     range = [0..numPiles-1]
     moves = moveStack <$> range <*> range
     flips = flipCard <$> range
     sets = moveToFoundation <$> range
+  pure $ moves ++ flips ++ sets
 
 distributed :: Iso' (a, Either b c) (Either (a, b) (a, c))
 distributed = iso to from
@@ -28,23 +27,24 @@ distributed = iso to from
     from (Left (a, b)) = (a, Left b)
     from (Right (a, c)) = (a, Right c)
 
-type Error = Either InvalidMove
+type MonadStack = ReaderT Env (Either InvalidMove)
 
-validSteps :: Game -> [Step]
-validSteps g =
+validSteps :: MonadReader Env m => Game -> m [Step]
+validSteps game = do
+  env <- ask
   let
-    paired = id &&& flip (moveReducer @Error) g
+    ms = runReader moves env
+    paired = id &&& (\move -> runReaderT (moveReducer @MonadStack move game) env)
     step = Step ^. from curried
-  in
-    moves ^.. folded . to paired . distributed . _Right . to step
+  pure $ ms ^.. folded . to paired . distributed . _Right . to step
 
 moveReducer
-  :: (MonadError InvalidMove m)
+  :: (MonadReader Env m, MonadError InvalidMove m)
   => Move
   -> Game
   -> m Game
 moveReducer move =
-  case normalize numPiles move of
+  case move of
     FlipCard (FC i) ->
       layout . _Layout . ix i $ \pile -> do
         if isJust $ pile ^? faceUp . _Cons
@@ -90,6 +90,14 @@ moveReducer move =
           $ MismatchingStacks i j
 
         pure $ layout & source' . target'
+
+normalize :: (MonadReader Env m) => Move -> m Move
+normalize move = do
+  n <- view env_numPiles
+  pure $ case move of
+    MoveStack (MS i j) -> moveStack (i `mod` n) (j `mod` n)
+    FlipCard (FC i) -> flipCard (i `mod` n)
+    MoveToFoundation (MTF i) -> moveToFoundation (i `mod` n)
 
 setSize :: Int
 setSize = enumSize @Card
--- test/SolitaireSpec.hs
+++ test/SolitaireSpec.hs
@@ -8,5 +8,13 @@ spec = do
   describe "Solitaire" $ do
     describe "deck" $ do
       it "should have the right number of cards" $ do
-        let deckSize = length deck
-        deckSize `shouldBe` (numCards * numCardCopies)
+        let env = Env { _env_numSets = 3, _env_numPiles = 3}
+        let runEnv = flip runReader env
+        let numCards = enumSize @Card
+        let
+          (deckSize, numCardCopies) = runEnv $ do
+            d <- length <$> getDeck
+            c <- view env_numSets
+            pure (d, c)
+        let product = numCards * numCardCopies :: Int
+        deckSize `shouldBe` product
