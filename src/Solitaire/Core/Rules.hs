4c69980afe2e1ab261efdfd5904765dc3879d283
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-09-14 19:09:12 -0400

    Work

--- src/Solitaire/Core/Rules.hs
+++ src/Solitaire/Core/Rules.hs
@@ -1,10 +1,22 @@
 {-# language UndecidableInstances #-}
+{-# language GADTs #-}
+{-# language DataKinds #-}
 module Solitaire.Core.Rules where
 
 import Solitaire.Prelude
 import Solitaire.Core.Move
 
 import qualified Data.HashSet as Set
+
+import Data.Proxy
+import GHC.OverloadedLabels (IsLabel(..))
+
+
+data SomeMoveType game = SomeMoveType
+  (forall move. IsMove move game => Proxy move)
+
+data SomeMove game = SomeMove
+  (forall move. IsMove move game => move)
 
 
 -- catch all constraint
@@ -13,8 +25,6 @@ type Solitaire rs =
   , Eq (Game rs)
   , Hashable (Game rs)
   , Pretty (Game rs)
-  , IsMove (Move rs) (Game rs) (InvalidMove rs)
-  , Exception (InvalidMove rs)
   , MonadReader (Config rs) (App rs)
   , MonadHistory (Game rs) (App rs)
   )
@@ -36,18 +46,17 @@ newtype MoveCount = MoveCount Int
   deriving stock (Eq, Ord)
   deriving newtype (Num)
 
-data GameConclusion rs = GameWon [Move rs] | GameLost
-deriving instance Show (Move rs) => Show (GameConclusion rs)
+data GameConclusion game = GameWon [SomeMove game] | GameLost
 
-data Step rs = Step
-  { move :: Move rs
-  , game :: Game rs
+data Step game = Step
+  { move :: SomeMove game
+  , game :: game
   }
   deriving (Generic)
 
-data GameWithPlayback rs = GameWithPlayback
-  { moves :: [Move rs]
-  , game :: Game rs
+data GameWithPlayback game = GameWithPlayback
+  { moves :: [SomeMove game]
+  , game :: game
   }
   deriving (Generic)
 
@@ -55,14 +64,23 @@ data GameWithPlayback rs = GameWithPlayback
 class Rules rs where
   type Config rs :: *
   type Game rs :: *
-  type Move rs :: *
-  type InvalidMove rs :: *
 
+  moveTypes :: [SomeMoveType (Game rs)]
+
+  -- TODO:
+  -- should be MonadPrimitive + MonadRandom instead of MonadIO
+  -- IO is used for mutable vectors & randomness to shuffle the deck
+  -- We don't need full IO
   newGame :: (MonadIO m, MonadReader (Config rs) m) => m (Game rs)
 
   gameIsWon :: Game rs -> Bool
 
-  moves :: (MonadReader (Config rs) m) => m [Move rs]
+allMoves :: forall rs m. (Rules rs, MonadReader (Config rs) m) => m [SomeMove (Game rs)]
+allMoves = reader $ \config -> do
+  SomeMoveType tmoveType <- moveTypes
+  undefined
+  --moves
+
 
 
 nextSteps
@@ -81,6 +99,6 @@ nextSteps game = do
     paired = id &&& (\move -> runReaderT (moveReducer @rs move game) config)
     step = Step ^. from curried
     unvisited = not . flip Set.member history . view #game
-    steps = runReader (moves @rs) config
+    steps = runReader (allMoves @rs) config
       ^.. folded . to paired . distributed . _Right . to step . filtered unvisited
   pure steps
